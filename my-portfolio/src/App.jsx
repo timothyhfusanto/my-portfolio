16116ae810ee05811c995f8332e466ca856f1cc1
Timothy Fusanto <e1070560@u.nus.edu>
2025-04-24 03:00:39 +0800

    fix

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
